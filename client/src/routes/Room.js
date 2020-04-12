5a8dcf188b3db055cc8c8e35856d0e02222ac0e7
Chaim <calvinfriedman@Calvins-MacBook-Pro-2.local>
2020-04-12 11:44:00 -0400

    first commit

---
+++ client/src/routes/Room.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    height: 100vh;
+    width: 90%;
+    margin: auto;
+    flex-wrap: wrap;
+`;
+
+const StyledVideo = styled.video`
+    height: 40%;
+    width: 50%;
+`;
+
+const Video = (props) => {
+    const ref = useRef();
+
+    useEffect(() => {
+        props.peer.on("stream", stream => {
+            ref.current.srcObject = stream;
+        })
+    }, []);
+
+    return (
+        <StyledVideo playsInline autoPlay ref={ref} />
+    );
+}
+
+
+const videoConstraints = {
+    height: window.innerHeight / 2,
+    width: window.innerWidth / 2
+};
+
+const Room = (props) => {
+    const [peers, setPeers] = useState([]);
+    const socketRef = useRef();
+    const userVideo = useRef();
+    const peersRef = useRef([]);
+    const roomID = props.match.params.roomID;
+
+    useEffect(() => {
+        socketRef.current = io.connect("/");
+        navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
+           
+        })
+    }, []);
+
+    function createPeer(userToSignal, callerID, stream) {
+       
+    }
+
+    function addPeer(incomingSignal, callerID, stream) {
+       
+    }
+
+    return (
+        <Container>
+            <StyledVideo muted ref={userVideo} autoPlay playsInline />
+            {peers.map((peer, index) => {
+                return (
+                    <Video key={index} peer={peer} />
+                );
+            })}
+        </Container>
+    );
+};
+
+export default Room;
